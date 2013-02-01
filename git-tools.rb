4e1e9352b4a6966171338b695ab1df41fad50093
Kenji Fujiwara <kenji-f@is.naist.jp>
2013-02-01 15:50:01 +0900

    apply brew audit

--- git-tools.rb
+++ git-tools.rb
@@ -4,13 +4,12 @@ require 'formula'
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
 class GitTools < ScriptFileFormula
-  homepage ''
+  homepage 'https://github.com/niyaton/git-tools'
   url 'https://raw.github.com/niyaton/git-tools/master/distiller.py'
   sha1 'cccc63a8f994a2a109783e06677ce7087888caf7'
   version '0.0.1'
 
-  # depends_on 'cmake' => :build
-  #depends_on :x11 # if your formula requires any X11/XQuartz components
+  depends_on 'findutils'
 
   def install
     bin.install 'distiller.py' => 'git-distiller.py'
